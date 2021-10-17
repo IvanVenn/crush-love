f83d8a94603ac2aafade72a1337c132760375404
IvanVenn <92671041+IvanVenn@users.noreply.github.com>
2021-10-17 17:49:46 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,7 +1,7 @@
 const CONFIGDATA = {
   titleHeader: "Làm người yêu tui nhé",
   descriptionHeader:
-    "Nếu bà thoát ra và không trả lời thì có nghĩa là ĐỒNG Ý rồi đấy ^^",
+    " Nếu bà thoát ra và không trả lời thì có nghĩa là bà đã ĐỒNG Ý rồi đấy ^^ ",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "Cảm ơn vì đã đồng ý",
